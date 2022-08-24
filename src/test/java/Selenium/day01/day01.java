7a520e3ff9eb8874461a182f197affaff74585cd
zhanzhaoyu <11292208+zhanzhaoyu@user.noreply.gitee.com>
2022-08-24 12:39:01 +0800

    Initial commit

--- src/test/java/Selenium/day01/day01.java
+++ src/test/java/Selenium/day01/day01.java
@@ -1,6 +1,5 @@
 package Selenium.day01;
 
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class day01 {
@@ -12,27 +11,11 @@ public class day01 {
 //        driver.get("https://www.baidu.com");
 //        driver.manage().window().maximize();
             //1、打开浏览器（edge）
-//            System.setProperty("webdriver.edge.driver","src/test/resources/msedgedriver.exe");
-//            EdgeDriver driver = new EdgeDriver();
-//            driver.get("https://www.baidu.com");
-//            driver.manage().window().maximize();
-
-            System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-            ChromeDriver driver = new ChromeDriver();
+            System.setProperty("webdriver.edge.driver","src/test/resources/msedgedriver.exe");
+            EdgeDriver driver = new EdgeDriver();
             driver.get("https://www.baidu.com");
             driver.manage().window().maximize();
 
-          //1、打开浏览器（chrome）--全新的浏览器，里面没有任何用户数据
-        //告诉代码指定驱动的路径
-//        System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
-//        ChromeDriver driver = new ChromeDriver();
-//        //2、访问网址
-//        driver.get("https://www.baidu.com");
-//        //3、将浏览器窗口最大化
-//        //链式调用
-//        driver.manage().window().maximize();
-//        //4、关闭浏览器
-//        driver.quit();
 
         }
 }
--- src/test/java/javaBase/com/lemon/RegisterTest.java
+++
@@ -1,9 +0,0 @@
-package javaBase.com.lemon;
-import org.testng.annotations.Test;
-
-public class RegisterTest {
-    @Test
-    public void RegisterTest1(){
-    }
-
-}
